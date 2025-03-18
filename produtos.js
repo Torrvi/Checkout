c629488f8c691b514ba0ae4b71aef2db6498ceba
Torrvi <Coelhozz24@gmail.com>
2025-03-18 15:19:25 -0300

    Ajustes

--- produtos.js
+++ produtos.js
@@ -4,7 +4,12 @@ carrosselContainers.forEach(container => {
     const carrossel = container.querySelector('.carrossel');
     const bantes = container.querySelector('.antes');
     const bdepois = container.querySelector('.depois');
-    const itemLargura = container.querySelector('.item').offsetWidth + 10;
+    const item = container.querySelector('.item');
+    
+    const carrosselStyle = getComputedStyle(carrossel);
+    const gap = parseInt(carrosselStyle.gap);
+    
+    const itemLargura = item.offsetWidth + gap;
 
     bdepois.addEventListener('click', () => {
         carrossel.scrollBy({ left: itemLargura, behavior: 'smooth' });
